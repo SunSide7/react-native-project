931950cf130081c7e7135608140c2c747cd37d2e
RS Developer <rsdeveloper@iMac-RS.local>
2019-06-17 11:45:24 +0200

    Lesson 2

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Restoraunt Review!
+          Restoraunt Review
         </Text>
       </View>
     );
