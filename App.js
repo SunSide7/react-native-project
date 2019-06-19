a9742be9b5727da65a1cf7a889aa5aa8893ffb01
RS Developer <rsdeveloper@iMac-RS.local>
2019-06-19 16:45:55 +0200

    Lesson 11: ScrollView

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import {
   View,
   Text,
   StyleSheet,
-  TextInput
+  TextInput,
+  ScrollView
 } from 'react-native'
 
 import Header from 'components/Header'
@@ -12,7 +13,23 @@ import Header from 'components/Header'
 const restaurants = [
   {name: 'React Cafe', address: '123 Anywhere St'},
   {name: 'Fancy Restaurant', address: '779 Main St'},
-  {name: 'Taco Place', address: '550 Maple Rd'}
+  {name: 'Taco Place', address: '550 Maple Rd'},
+  {name: "Tony's Dinner", address: '4101 College St'},
+  {name: 'Pasta Central', address: '706 Harper St'},
+  {name: 'Burger Builder', address: '4869 Hamilton Dr'},
+  {name: 'Pizza Express', address: '1049 Bird St'},
+  {name: 'Teriaki To Go', address: '1885 Tea Berry Lane'},
+  {name: 'Maroon Deli', address: '1082 Stuart St'},
+  {name: 'Prime Bar and Grill', address: '1848 Fairfax Dr'},
+  {name: 'Dumping House', address: '747 Kelly Dr'},
+  {name: 'Hot Chicken', address: '1816 Olive St'},
+  {name: "Luna's Tap Room", address: '3256 Cherry Ridge Dr'},
+  {name: "Bobby's Burgers", address: '4152 Berkley St'},
+  {name: 'Turnpike Diner', address: '4571 Central Ave'},
+  {name: 'Bombay Express', address: '65 Queens Lane'},
+  {name: 'Coffe Central', address: '3228 Oakwood Circle'},
+  {name: "King's Garden", address: '2935 Victoria St'},
+  {name: 'Salads and More', address: '2454 Preston St'}
 ]
 
 export default class App extends Component {
@@ -37,36 +54,39 @@ export default class App extends Component {
           }}
           value={this.state.search}/>
 
+        <ScrollView contentContainerStyle={{
+          paddingTop: 30
+        }}>
+          {
+            restaurants.filter(place => {
+              return !this.state.search ||
+                    place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
+            }).map((place, index) => {
+              return (
+                <View key={place.name} style={[
+                    styles.row,
+                    {backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'}
+                  ]}  >
+                  <View style={styles.edges}>
+                    <Text>{index + 1}</Text>
+                  </View>
 
-        {
-          restaurants.filter(place => {
-            return !this.state.search ||
-                  place.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1
-          }).map((place, index) => {
-            return (
-              <View key={place.name} style={[
-                  styles.row,
-                  {backgroundColor: index % 2 === 0 ? 'white' : '#F3F3F7'}
-                ]}  >
-                <View style={styles.edges}>
-                  <Text>{index + 1}</Text>
-                </View>
+                  <View style={{
+                    flexDirection: 'column',
+                    flex: 8
+                  }}>
+                    <Text>{place.name}</Text>
+                    <Text style={styles.addressText}>{place.address}</Text>
+                  </View>
 
-                <View style={{
-                  flexDirection: 'column',
-                  flex: 8
-                }}>
-                  <Text>{place.name}</Text>
-                  <Text style={styles.addressText}>{place.address}</Text>
+                  <View style={styles.nameAddress}>
+                    <Text>Info</Text>
+                  </View>
                 </View>
-
-                <View style={styles.nameAddress}>
-                  <Text>Info</Text>
-                </View>
-              </View>
-            )
-          })
-        }
+              )
+            })
+          }          
+        </ScrollView>
 
       </View>
     );
@@ -93,7 +113,6 @@ const styles = StyleSheet.create({
     color: 'gray'
   },
   input: {
-    marginBottom: 30,
     padding: 10,
     paddingHorizontal: 20,
     fontSize: 16,
